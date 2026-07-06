80fbe14f0b9b969b3c7c63d20845b8a125ba0fe2
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-06 19:20:30 +0500

    add create_app function and init the app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 
 from app.infrastructure.config import get_settings
+from app.presentation.api.handlers import register_exception_handlers
+
 
 def create_app() -> FastAPI:
     settings = get_settings()
 
     app = FastAPI(
-        title=settings.app_name,
-        debug=settings.debug,
+        title=settings.api.title,
+        debug=settings.api.debug,
     )
+    register_exception_handlers(app)
     return app
+
 
 app = create_app()
