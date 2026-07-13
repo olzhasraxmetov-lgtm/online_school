fb87203dad911e03b247662fc38786dadd7dc3ff
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-13 14:49:45 +0500

    feat(repository): add CourseRepository with 2 abstract methods

---
+++ app/application/interfaces/repositories/course_repository.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from app.domain.entities.course import Course
+
+
+class CourseRepository(ABC):
+    @abstractmethod
+    async def get_by_id(self, course_id: UUID) -> Course | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def list(self) -> 'list[Course]':
+        raise NotImplementedError
