cfca0174022725d35fd76260658906c8448b4e36
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-13 15:40:22 +0500

    feat(lecture): implement GetLecturesUseCase - Add execute method with if lecture is existing

---
+++ app/application/use_cases/lectures/get_lecture.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+from app.application.exceptions import LectureNotFoundError
+from app.application.interfaces.repositories.lecture_repository import LectureRepository
+from app.domain.entities.lecture import Lecture
+
+
+@dataclass(slots=True)
+class GetLectureQuery:
+    lecture_id: UUID
+
+class GetLecturesUseCase:
+    def __init__(self, lecture_repository: LectureRepository) -> None:
+        self.lecture_repository = lecture_repository
+
+    async def execute(self, query: GetLectureQuery) -> Lecture | None:
+        lecture = await self.lecture_repository.get_by_id(query.lecture_id)
+        if lecture is None:
+            raise LectureNotFoundError("Lecture not found")
+        return lecture
