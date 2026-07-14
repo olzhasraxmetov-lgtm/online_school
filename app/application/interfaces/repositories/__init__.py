0e658e073b997c610e6030331e185630f5175b36
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-14 16:44:48 +0500

    chore: add __init__.py in repositories directory for imports

---
+++ app/application/interfaces/repositories/__init__.py
@@ -0,0 +1,12 @@
+from app.application.interfaces.repositories.course_repository import CourseRepository
+from app.application.interfaces.repositories.lecture_repository import LectureRepository
+from app.application.interfaces.repositories.module_repository import ModuleRepository
+from app.application.interfaces.repositories.section_repository import SectionRepository
+
+__all__ = [
+    "LectureRepository",
+    "SectionRepository",
+    "ModuleRepository",
+    "SectionRepository",
+]
+
