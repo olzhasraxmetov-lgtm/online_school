eac75e5d02c92db79001a6810ff375cdde607c01
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-18 16:17:47 +0500

    feat(routers): add GET endpoints for courses - add /course - add /course/{course_id} - add /course/{course_id}/structure - add /lectures/{lecture_id}

--- app/presentation/api/routes/content.py
+++ app/presentation/api/routes/content.py
@@ -1,3 +1,61 @@
-from fastapi import APIRouter
+from uuid import UUID
 
-router = APIRouter(tags=["content"])
+from fastapi import APIRouter, Depends
+
+from app.application.use_cases.courses.get_course import GetCourseUseCase, GetCourseQuery
+from app.application.use_cases.courses.get_course_structure import GetCourseStructureUseCase, GetCourseStructureQuery
+from app.application.use_cases.courses.get_courses import GetCoursesUseCase, GetCoursesQuery
+from app.application.use_cases.lectures.get_lecture import GetLectureUseCase, GetLectureQuery
+from app.presentation.api.dependencies import (
+    get_get_course_use_case, get_get_courses_use_case, get_get_course_structure_use_case, get_get_lecture_use_case,
+)
+from app.presentation.api.schemas import (
+    CourseListItemResponse,
+)
+from app.presentation.api.schemas.content.course import CourseResponse, CourseStructureResponse
+from app.presentation.api.schemas.content.lecture import LectureResponse
+
+router = APIRouter(tags=["Content"])
+
+@router.get(
+    "/courses",
+    response_model=list[CourseListItemResponse],
+)
+async def get_courses(
+        use_case: GetCoursesUseCase = Depends(get_get_courses_use_case),
+) -> list[CourseListItemResponse]:
+    result = await use_case.execute(GetCoursesQuery())
+    return [CourseListItemResponse.model_validate(course) for course in result]
+
+@router.get(
+    "/courses/{course_id}",
+    response_model=CourseResponse,
+)
+async def get_course(
+        course_id: UUID,
+        use_case: GetCourseUseCase = Depends(get_get_course_use_case),
+) -> CourseResponse:
+    result  = await use_case.execute(GetCourseQuery(course_id))
+    return CourseResponse.model_validate(result)
+
+@router.get(
+    "/courses/{course_id}/structure}",
+    response_model=CourseStructureResponse,
+)
+async def get_courses(
+        course_id: UUID,
+        use_case: GetCourseStructureUseCase = Depends(get_get_course_structure_use_case),
+) -> CourseStructureResponse:
+    result = await use_case.execute(GetCourseStructureQuery(course_id))
+    return CourseStructureResponse.model_validate(result)
+
+@router.get(
+    "/lectures/{lecture_id}",
+    response_model=LectureResponse,
+)
+async def get_lecture(
+        lecture_id: UUID,
+        use_case: GetLectureUseCase = Depends(get_get_lecture_use_case),
+) -> LectureResponse:
+    result = await use_case.execute(GetLectureQuery(lecture_id=lecture_id))
+    return LectureResponse.model_validate(result)
