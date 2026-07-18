d11da9cb5826893a48b895e1fd6b08aeeaa6fbef
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-18 14:57:58 +0500

    feat(schemas): add schemas for main entities and add them to __init__.py

--- app/presentation/api/schemas/__init__.py
+++ app/presentation/api/schemas/__init__.py
@@ -1,3 +1,21 @@
+from app.presentation.api.schemas.content import (
+    CourseListItemResponse,
+    CourseResponse,
+    CourseStructureResponse,
+    LectureResponse,
+    LectureStructureResponse,
+    ModuleStructureResponse,
+    SectionStructureResponse,
+)
 from app.presentation.api.schemas.errors import ErrorResponse
 
-__all__ = ['ErrorResponse']
+__all__ = [
+    "CourseListItemResponse",
+    "CourseResponse",
+    "CourseStructureResponse",
+    "LectureResponse",
+    "LectureStructureResponse",
+    "ModuleStructureResponse",
+    "SectionStructureResponse",
+    "ErrorResponse",
+]
