705e77045cfe9d50e4ca37e5eaf9119ef127ee25
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-18 16:37:35 +0500

    feat(schemas): add dependencies for update and create use cases

--- app/presentation/api/dependencies.py
+++ app/presentation/api/dependencies.py
@@ -2,10 +2,18 @@ from collections.abc import AsyncIterator
 
 from fastapi import Depends
 
+from app.application.use_cases.courses.create_course import CreateCourseUseCase
 from app.application.use_cases.courses.get_course import GetCourseUseCase
 from app.application.use_cases.courses.get_course_structure import GetCourseStructureUseCase
 from app.application.use_cases.courses.get_courses import GetCoursesUseCase
+from app.application.use_cases.courses.update_course import UpdateCourseUseCase
+from app.application.use_cases.lectures.create_lecture import CreateLectureUseCase
 from app.application.use_cases.lectures.get_lecture import GetLectureUseCase
+from app.application.use_cases.lectures.update_lecture import UpdateLectureUseCase
+from app.application.use_cases.modules.create_module import CreateModuleUseCase
+from app.application.use_cases.modules.update_module import UpdateModuleUseCase
+from app.application.use_cases.sections.create_section import CreateSectionUseCase
+from app.application.use_cases.sections.update_section import UpdateSectionUseCase
 from app.infrastructure.database import SessionFactory, SqlAlchemyUnitOfWork
 
 
@@ -37,3 +45,43 @@ def get_get_lecture_use_case(
     uow: SqlAlchemyUnitOfWork = Depends(get_uow),
 ) -> GetLectureUseCase:
     return GetLectureUseCase(lecture_repository=uow.lectures)
+
+def get_create_course_use_case() -> CreateCourseUseCase:
+    return CreateCourseUseCase(
+        uow=SqlAlchemyUnitOfWork(session_factory=SessionFactory),
+    )
+def get_update_course_use_case() -> UpdateCourseUseCase:
+    return UpdateCourseUseCase(
+        uow=SqlAlchemyUnitOfWork(session_factory=SessionFactory)
+    )
+
+def get_create_module_use_case() -> CreateModuleUseCase:
+    return CreateModuleUseCase(
+        uow=SqlAlchemyUnitOfWork(session_factory=SessionFactory)
+    )
+
+def get_update_module_use_case() -> UpdateModuleUseCase:
+    return UpdateModuleUseCase(
+        uow=SqlAlchemyUnitOfWork(session_factory=SessionFactory)
+    )
+
+
+def get_create_section_use_case() -> CreateSectionUseCase:
+    return CreateSectionUseCase(
+        uow=SqlAlchemyUnitOfWork(session_factory=SessionFactory)
+    )
+
+def get_update_section_use_case() -> UpdateSectionUseCase:
+    return UpdateSectionUseCase(
+        uow=SqlAlchemyUnitOfWork(session_factory=SessionFactory)
+    )
+
+def get_create_lecture_use_case() -> CreateLectureUseCase:
+    return CreateLectureUseCase(
+        uow=SqlAlchemyUnitOfWork(session_factory=SessionFactory)
+    )
+
+def get_update_lecture_use_case() -> UpdateLectureUseCase:
+    return UpdateLectureUseCase(
+        uow=SqlAlchemyUnitOfWork(session_factory=SessionFactory)
+    )
