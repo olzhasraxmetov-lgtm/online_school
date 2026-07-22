7d32314c3242c5c7dbf9ec52ca751828fe521ada
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-22 20:19:53 +0500

    feat(schemas): add schemas for user logging and registration

---
+++ app/presentation/api/schemas/auth.py
@@ -0,0 +1,26 @@
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+
+from app.domain.entities.user import UserRole
+
+
+class RegisterUserRequest(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=128)
+
+class RegisteredUserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    email: EmailStr
+    role: UserRole
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=1, max_length=128)
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
