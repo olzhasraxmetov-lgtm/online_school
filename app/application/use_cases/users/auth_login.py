715e350f857a5dbe2bcfbcc2faac76cf9dd2c596
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-22 19:51:18 +0500

    feat(user use cases): rewrite execute method with creating a token

--- app/application/use_cases/users/auth_login.py
+++ app/application/use_cases/users/auth_login.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
+from app.application.dto.auth_token import AuthToken
 from app.application.exceptions import InvalidCredentialsError
 from app.application.interfaces.services.password_hasher import PasswordHasher
+from app.application.interfaces.services.token_service import TokenService
 from app.application.interfaces.unit_of_work import UnitOfWork
 from app.domain.entities.user import User
 
@@ -11,11 +13,17 @@ class LoginUserCommand:
     password: str
 
 class LoginUserUseCase:
-    def __init__(self, uow: UnitOfWork, password_hasher: PasswordHasher) -> None:
+    def __init__(
+            self,
+            uow: UnitOfWork,
+            password_hasher: PasswordHasher,
+            token_service: TokenService,
+    ) -> None:
         self.uow = uow
         self.password_hasher = password_hasher
+        self.token_service = token_service
 
-    async def execute(self, command: LoginUserCommand) -> User:
+    async def execute(self, command: LoginUserCommand) -> AuthToken:
         async with self.uow:
             user = await self.uow.users.get_by_email(command.email)
             if not user:
@@ -27,4 +35,10 @@ class LoginUserUseCase:
             )
             if not is_valid_password:
                 raise InvalidCredentialsError("Invalid email or password")
-            return user
+
+            access_token = self.token_service.create_access_token(
+                user_id=user.id,
+                role=user.role.value
+            )
+
+            return AuthToken(access_token=access_token)
