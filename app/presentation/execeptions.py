4c63bc1eab2d90b5ca119aa041cafb25b9f7ab99
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-23 19:25:53 +0500

    feat(exceptions): add AuthenticationError and PermissionDeniedError

--- app/presentation/execeptions.py
+++ app/presentation/execeptions.py
@@ -1,2 +1,8 @@
 class PresentationError(Exception):
     """Base exception for presentation-layer errors."""
+
+class AuthenticationError(PresentationError):
+    pass
+
+class PermissionDeniedError(PresentationError):
+    pass
