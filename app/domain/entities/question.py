4bc8d5df7ea18119f6e89abcdb9aecfcb2e2eb33
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-07-31 12:53:59 +0500

    feat(domain entities): extend Question entity with type of question and methods - add QuestionType: SINGLE_CHOICE or MULTIPLE_CHOICE - add question_type field to Question - add validate_answer_options_configuration method to check options

--- app/domain/entities/question.py
+++ app/domain/entities/question.py
@@ -1,7 +1,16 @@
-from  dataclasses import dataclass, field
+import typing
+from dataclasses import dataclass, field
+from typing import Sequence
 from uuid import UUID
 
 from app.domain.exceptions import InvalidQuestionError
+
+if typing.TYPE_CHECKING:
+    from app.domain.entities.answer_option import AnswerOption
+
+class QuestionType:
+    SINGLE_CHOICE = 'single_choice'
+    MULTIPLE_CHOICE = 'multiple_choice'
 
 @dataclass(slots=True)
 class Question:
@@ -9,6 +18,7 @@ class Question:
     section_id: UUID
     text: str
     position: int
+    question_type: QuestionType = QuestionType.SINGLE_CHOICE
     answer_option_ids: list[UUID] = field(default_factory=list)
     max_attempts: int = 1
     reward_points: int = 1
@@ -28,6 +38,28 @@ class Question:
 
         if self.reward_points < 1:
             raise InvalidQuestionError("Question reward points must be positive.")
+
+    def validate_answer_options_configuration(
+            self,
+            answer_options: Sequence['AnswerOption']
+    ) -> None:
+        if len(answer_options) < 2:
+            raise InvalidQuestionError("Question must have at least two answer options.")
+
+        correct_options_count = sum(1 for option in answer_options if option.is_correct)
+
+        if correct_options_count == 0:
+            raise InvalidQuestionError("Question must have at least one answer option.")
+
+        if self.is_single_choice() and correct_options_count != 1:
+            raise InvalidQuestionError(
+                "Single chose question must have exactly one answer option."
+            )
+
+        if self.is_multiple_choice() and correct_options_count < 2:
+            raise InvalidQuestionError(
+                "Multiple chose question must have at least two correct answer options."
+            )
 
     def update(
             self,
@@ -49,5 +81,14 @@ class Question:
         if answer_option_id in self.answer_option_ids:
             self.answer_option_ids.remove(answer_option_id)
 
-    def has_answer_options(self, answer_option_id: UUID) -> bool:
+    def has_answer_options(self) -> bool:
         return bool(self.answer_option_ids)
+
+    def is_single_choice(self) -> bool:
+        return self.question_type is QuestionType.SINGLE_CHOICE
+
+    def is_multiple_choice(self) -> bool:
+        return self.question_type is QuestionType.MULTIPLE_CHOICE
+
+    def allows_multiple_answers(self) -> bool:
+        return self.is_multiple_choice()
