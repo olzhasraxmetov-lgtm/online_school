ad7ab52016ed4c72ff7861c6d16089039e080dc0
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-06 19:56:30 +0500

    feat(use cases): add checking from CourseAccessService for answer_options

--- app/application/use_cases/answer_options/create_answer_option.py
+++ app/application/use_cases/answer_options/create_answer_option.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 from uuid import UUID, uuid4
 
-from app.application.exceptions import PermissionDeniedError, QuestionNotFoundError, QuestionAlreadyUsedError
+from app.application.exceptions import QuestionNotFoundError, QuestionAlreadyUsedError
 from app.application.interfaces.unit_of_work import UnitOfWork
+from app.application.services.course_access_service import CourseAccessService
 from app.domain.entities.answer_option import AnswerOption
 from app.domain.entities.user import User
 
@@ -18,15 +19,18 @@ class CreateAnswerOptionCommand:
 class CreateAnswerOptionUseCase:
     def __init__(self, uow: UnitOfWork) -> None:
         self.uow = uow
+        self.course_access_service = CourseAccessService(uow)
 
     async def execute(self, command: CreateAnswerOptionCommand) -> AnswerOption:
-        if not command.actor.can_manage_interactive_content():
-            raise PermissionDeniedError("User cannot manage interactive content.")
-
         async with self.uow:
             question = await self.uow.questions.get_by_id(command.question_id)
             if question is None:
                 raise QuestionNotFoundError(f"Question not found")
+
+            await self.course_access_service.ensure_can_manage_section(
+                actor=command.actor,
+                section_id=question.section_id,
+            )
 
             has_attempts = await self.uow.question_attempts.exists_by_question_id(question.id)
             if has_attempts:
--- app/application/use_cases/answer_options/update_answer_option.py
+++ app/application/use_cases/answer_options/update_answer_option.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from uuid import UUID
 
-from app.application.exceptions import AnswerOptionNotFoundError, PermissionDeniedError, QuestionAlreadyUsedError
+from app.application.exceptions import AnswerOptionNotFoundError, PermissionDeniedError, QuestionAlreadyUsedError, \
+    QuestionNotFoundError
 from app.application.interfaces.unit_of_work import UnitOfWork
+from app.application.services.course_access_service import CourseAccessService
 from app.domain.entities.answer_option import AnswerOption
 from app.domain.entities.user import User
 
@@ -18,15 +20,22 @@ class UpdateAnswerOptionCommand:
 class UpdateAnswerOptionUseCase:
     def __init__(self, uow: UnitOfWork):
         self.uow = uow
+        self.course_access_service = CourseAccessService(uow)
 
     async def execute(self, command: UpdateAnswerOptionCommand) -> AnswerOption:
-        if not command.actor.can_manage_interactive_content():
-            raise PermissionDeniedError('User cannot manage interactive content.')
-
         async with self.uow:
             answer_option = await self.uow.answer_options.get_by_id(command.answer_option_id)
             if answer_option is None:
                 raise AnswerOptionNotFoundError('Answer option not found.')
+
+            question = await self.uow.questions.get_by_id(answer_option.question_id)
+            if question is None:
+                raise QuestionNotFoundError('Question not found.')
+
+            await self.course_access_service.ensure_can_manage_section(
+                actor=command.actor,
+                section_id=question.section_id,
+            )
 
             has_attempts = await self.uow.question_attempts.exists_by_question_id(answer_option.question_id)
             if has_attempts:
