dc454a34d528e50ecd14064955783494dd35c64c
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-07 19:14:42 +0500

    feat(repositories): extend QuestionAttemptRepository by methods: get_by_student_and_question, get_by_id and add

--- app/application/interfaces/repositories/question_attempt_repository.py
+++ app/application/interfaces/repositories/question_attempt_repository.py
@@ -1,8 +1,26 @@
 from abc import abstractmethod, ABC
 from uuid import UUID
 
+from app.domain.entities import QuestionAttempt
+
 
 class QuestionAttemptRepository(ABC):
     @abstractmethod
+    async def get_by_id(self, attempt_id: UUID) -> QuestionAttempt | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_student_and_question(
+            self,
+            student_id: UUID,
+            question_id: UUID
+    ) -> list[QuestionAttempt]:
+        raise NotImplementedError
+
+    @abstractmethod
     async def exists_by_question_id(self, question_id: UUID) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def add(self, attempt: QuestionAttempt) -> None:
         raise NotImplementedError
