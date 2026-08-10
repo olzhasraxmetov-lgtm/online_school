fa61a86db9db7f2afd6e34ce9ebb33d8cfcb4f50
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-10 16:24:40 +0500

    chore: add mappers to __init__.py

--- app/infrastructure/database/mappers/__init__.py
+++ app/infrastructure/database/mappers/__init__.py
@@ -1,6 +1,10 @@
+from app.infrastructure.database.mappers.answer_option_mapper import AnswerOptionMapper
 from app.infrastructure.database.mappers.course_mapper import CourseMapper
 from app.infrastructure.database.mappers.lecture_mapper import LectureMapper
 from app.infrastructure.database.mappers.module_mapper import ModuleMapper
+from app.infrastructure.database.mappers.progress_mapper import ProgressMapper
+from app.infrastructure.database.mappers.question_attempt_mapper import QuestionAttemptMapper
+from app.infrastructure.database.mappers.question_mapper import QuestionMapper
 from app.infrastructure.database.mappers.section_mapper import SectionMapper
 from app.infrastructure.database.mappers.user_mapper import UserMapper
 
@@ -10,4 +14,8 @@ __all__ = [
     "SectionMapper",
     "LectureMapper",
     "UserMapper",
+    "ProgressMapper",
+    "QuestionMapper",
+    "QuestionAttemptMapper",
+    "AnswerOptionMapper",
 ]
