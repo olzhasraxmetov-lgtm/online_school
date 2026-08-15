572adcdb209920448c24d6a5b8130fae2badacfd
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-15 12:02:55 +0500

    chore: add interactive_router to __init__.py

--- app/presentation/api/routes/__init__.py
+++ app/presentation/api/routes/__init__.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter
 
 from app.infrastructure.config import get_settings
 from app.presentation.api.routes.admin import router as admin_router
-from app.presentation.api.routes.content import router as content_router
 from app.presentation.api.routes.auth import router as auth_router
+from app.presentation.api.routes.content import router as content_router
+from app.presentation.api.routes.interactive_admin import router as interactive_admin_router
 
 settings = get_settings()
 
@@ -11,3 +12,4 @@ router = APIRouter(prefix=settings.api_prefix)
 router.include_router(content_router)
 router.include_router(admin_router)
 router.include_router(auth_router)
+router.include_router(interactive_admin_router)
