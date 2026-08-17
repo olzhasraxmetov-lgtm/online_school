6731a453cb4bd6066683792d1e35b6d93a0e7072
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-17 19:54:57 +0500

    feat(tests): add author_id param for all Course domain entities

--- tests/unit/domain/test_course.py
+++ tests/unit/domain/test_course.py
@@ -5,9 +5,11 @@ import pytest
 from app.domain.entities.course import Course
 from app.domain.exceptions import InvalidCourseError
 
+
 def test_course_is_created_with_valid_data() -> None:
     course = Course(
         id=uuid4(),
+        author_id=uuid4(),
         title="SQLAlchemy",
         description="SQLAlchemy practicing",
     )
@@ -19,6 +21,7 @@ def test_course_raises_error_when_title_is_blank() -> None:
     with pytest.raises(InvalidCourseError):
         Course(
             id=uuid4(),
+            author_id=uuid4(),
             title="",
             description="SQLAlchemy practicing",
         )
@@ -27,6 +30,7 @@ def test_course_raises_error_when_description_is_blank() -> None:
     with pytest.raises(InvalidCourseError):
         Course(
             id=uuid4(),
+            author_id=uuid4(),
             title="Valid title",
             description="",
         )
@@ -34,6 +38,7 @@ def test_course_raises_error_when_description_is_blank() -> None:
 def test_course_update_changes_title() -> None:
     course = Course(
         id=uuid4(),
+        author_id=uuid4(),
         title="SQLAlchemy",
         description="SQLAlchemy practicing",
     )
