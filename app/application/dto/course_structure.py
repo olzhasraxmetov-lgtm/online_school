f78fc8659b4a8d505f569f6a9ca01548edf51c10
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-24 20:40:07 +0500

    feat(dto): add task_ids param for SectionStructureDTO

--- app/application/dto/course_structure.py
+++ app/application/dto/course_structure.py
@@ -14,6 +14,7 @@ class SectionStructureDTO:
     description: str
     position: int
     question_ids: list[UUID] = field(default_factory=list)
+    task_ids: list[UUID] = field(default_factory=list)
     lectures: list[LectureStructureDTO] = field(default_factory=list)
 
 @dataclass(slots=True)
