6b675d44041401af4d5c7af4b4f82a8b27e21d27
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-30 15:43:56 +0500

    feat(mappers): add completed_task_ids field for both methods: to_domain and to_model

--- app/infrastructure/database/mappers/progress_mapper.py
+++ app/infrastructure/database/mappers/progress_mapper.py
@@ -12,6 +12,7 @@ class ProgressMapper:
             student_id=UUID(model.student_id),
             course_id=UUID(model.course_id),
             completed_question_ids=[UUID(item) for item in model.completed_question_ids],
+            completed_task_ids=[UUID(item) for item in model.completed_task_ids],
             completed_section_ids=[UUID(item) for item in model.completed_section_ids],
             completed_module_ids=[UUID(item) for item in model.completed_module_ids],
             total_points=model.total_points,
@@ -25,6 +26,7 @@ class ProgressMapper:
             course_id=str(entity.course_id),
             completed_question_ids=[str(item) for item in entity.completed_question_ids],
             completed_section_ids=[str(item) for item in entity.completed_section_ids],
+            completed_task_ids=[str(item) for item in entity.completed_task_ids],
             completed_module_ids=[str(item) for item in entity.completed_module_ids],
             total_points=entity.total_points,
         )
