3272b96b4152a8a2093d7eeb9294a079d071edce
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-31 15:19:12 +0500

    feat(interfaces): add new abstract method dequeue to SubmissionQueue

--- app/application/interfaces/submission_queue.py
+++ app/application/interfaces/submission_queue.py
@@ -6,3 +6,7 @@ class SubmissionQueue(ABC):
     @abstractmethod
     async def enqueue(self, submission_id: UUID) -> None:
         raise NotImplementedError
+
+    @abstractmethod
+    async def dequeue(self) -> UUID:
+        raise NotImplementedError
