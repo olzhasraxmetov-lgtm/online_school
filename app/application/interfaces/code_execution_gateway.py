01c5b5cefb94de294d2da38d48f4a572486d3e7e
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-08-31 15:44:52 +0500

    feat(application interfaces): add code_task param to  execute

--- app/application/interfaces/code_execution_gateway.py
+++ app/application/interfaces/code_execution_gateway.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from app.domain.entities.code_submission import CodeSubmission
+from app.domain.entities.code_task import CodeTask
 from app.domain.entities.execution_result import ExecutionResult
 from app.domain.entities.test_case import TestCase
 
@@ -9,6 +10,7 @@ class CodeExecutionGateway(ABC):
     @abstractmethod
     async def execute(
         self,
+        code_task: CodeTask,
         submission: CodeSubmission,
         test_cases: list[TestCase],
     ) -> ExecutionResult:
