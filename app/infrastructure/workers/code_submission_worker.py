1dfe971d665df187d26355b6009fd7021d1377b0
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-09-01 19:55:47 +0500

    feat(workers): add RetryableExecutionError handle exception in try/except

--- app/infrastructure/workers/code_submission_worker.py
+++ app/infrastructure/workers/code_submission_worker.py
@@ -1,5 +1,6 @@
 import logging
 
+from app.application.exceptions import RetryableExecutionError
 from app.application.interfaces.submission_queue import SubmissionQueue
 from app.application.use_cases.code_submissions.process_code_submission import (
     ProcessCodeSubmissionCommand,
@@ -25,7 +26,12 @@ class CodeSubmissionWorker:
                 await self.process_use_case.execute(
                     ProcessCodeSubmissionCommand(submission_id=submission_id)
                 )
-
+            except RetryableExecutionError:
+                logger.warning(
+                    'Code submission processing will be retried',
+                    extra={'submission_id': str(submission_id)},
+                )
+                await self.queue.enqueue(submission_id)
             except Exception:
                 logger.exception(
                     'Code submission processing failed',
